cfc9617134b8fc1f27e7e60585e39c01465adc87
Pedr0ALS <142238296+Pedr0ALS@users.noreply.github.com>
2024-02-26 17:31:24 +0000

    Modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Mesmo em outra encarnação, todo mundo já foi Flamengo, quem nunca foi Flamengo nunca existiu.</h1>
+    return <h1>Mesmo em outra encarnação, todo mundo já foi Flamengo, quem nunca foi Flamengo nunca existiu. ⚽ </h1>
 }
 
 export default Home;
