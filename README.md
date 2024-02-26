7574103e5032c2986f1cf9938eeb8b977f2a796e
Pedr0ALS <142238296+Pedr0ALS@users.noreply.github.com>
2024-02-26 22:11:09 +0000

    adiciona scripts `lint:check` e `lint:write`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnewss
+
 Implementação do https://www.tabnews.com.br para o curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home () {
-    return <h1>Mesmo em outra encarnação, todo mundo já foi Flamengo, quem nunca foi Flamengo nunca existiu. ⚽ </h1>
+function Home() {
+  return (
+    <h1>
+      Mesmo em outra encarnação, todo mundo já foi Flamengo, quem nunca foi
+      Flamengo nunca existiu. ⚽{" "}
+    </h1>
+  );
 }
 
 export default Home;
